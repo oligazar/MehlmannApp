8305d4410219f50c235a3f46fda3787d108a2d38
Alexander <nikelodian@gmail.com>
2020-12-18 10:45:33 +0200

    Android migration to FlutterFire with embedding v2 succeeded

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,16 @@ import 'package:flutter/material.dart';
 import 'package:mahlmann_app/app_mahlmann.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-// import 'package:flutter/foundation.dart' show kDebugMode;
+import 'package:flutter/foundation.dart' show kDebugMode;
 
 void main() async {
 	WidgetsFlutterBinding.ensureInitialized();
 	
   await Firebase.initializeApp();
-  // if (kDebugMode) {
-  //   await FirebaseCrashlytics.instance
-  //       .setCrashlyticsCollectionEnabled(false);
-  // }
+  if (kDebugMode) {
+    await FirebaseCrashlytics.instance
+        .setCrashlyticsCollectionEnabled(false);
+  }
 	runZonedGuarded<Future<void>>(() async {
     runApp(AppMahlmann());
 	}, FirebaseCrashlytics.instance.recordError);
